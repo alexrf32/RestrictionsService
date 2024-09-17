c599ff43051dc0e95995fcf324fe312581f1323f
alexrf32 <72366671+alexrf32@users.noreply.github.com>
2024-09-16 23:30:02 -0300

    feat: create Controllers folder and add RestrictionsController.cs

---
+++ Controllers/RestrictionsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using RestrictionService.Models;
+using RestrictionService.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestrictionService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RestrictionsController : ControllerBase
+    {
+        private readonly FirestoreService _firestoreService;
+
+        public RestrictionsController(FirestoreService firestoreService)
+        {
+            _firestoreService = firestoreService;
+        }
+
+        [HttpGet("{studentId}")]
+        public async Task<ActionResult<List<Restriction>>> GetRestrictions(string studentId)
+        {
+            var restrictions = await _firestoreService.GetRestrictions(studentId);
+            return Ok(restrictions);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AssignRestriction(Restriction restriction)
+        {
+            restriction.AssignedAt = Timestamp.GetCurrentTimestamp();
+            await _firestoreService.AssignRestriction(restriction);
+            return Ok();
+        }
+    }
+}
