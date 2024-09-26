44ea25123b7755ce9e32db33bc8b22637efb49f1
alexrf32 <72366671+alexrf32@users.noreply.github.com>
2024-09-26 17:58:28 -0300

    chore: update program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddSingleton<RestrictionService.Services.FirestoreService>();
 
 
@@ -10,19 +9,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
 if (app.Environment.IsDevelopment())
 {
-   
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-
 app.MapControllers();
-
 app.Run();
